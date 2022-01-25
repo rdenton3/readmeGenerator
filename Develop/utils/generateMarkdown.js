17e83c90e8c4dcfcd0f976a7b07fcf7259d50031
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-01-25 11:50:16 -0500

    populated readme template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,10 +2,6 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
@@ -14,6 +10,26 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ##Description
+  ${data.description}
+
+  ##Usage
+  ${data.usage}
+
+  ##Installation
+  ${data.install}
+
+  ##Tests
+  ${data.test}
+
+  ##Contributions
+  ${data.contribute}
+
+  ##License
+  ${data.license}
+
+  ##Questions
+  ${data.questions}
 `;
 }
 
